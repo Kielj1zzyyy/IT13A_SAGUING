a57efb012f899f646622676735a50043f9354042
misspatriana <saguingaaronezekiel@gmail.com>
2025-02-18 21:08:07 +0800

    java file
    Added HelloWorld class

--- IT13A_SAGUING/src/Prelim/Lab_Act1_Arithmetics.java
+++ IT13A_SAGUING/src/Prelim/Lab_Act1_Arithmetics.java
@@ -1,13 +1,27 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Prelim;
 
-/**
- *
- * @author Administrator
- */
 public class Lab_Act1_Arithmetics {
-    
+
+    public static void main(String[] org) {
+
+        int numb1 = 10;
+        int numb2 = 6;
+        int numb3 = 4;
+        int numb4 = 3;
+
+        int firstnum1 = numb1 * numb3 + numb2;
+        int saguing1 = numb1 - numb3;
+        int firstnum2 = saguing1 % numb2;
+        int saguing2 = numb1 + numb3 + numb2;
+        int firstnum3 = saguing2 / numb4;
+        int saguing3 = numb3 * numb3;
+        int firstnum4 = numb1 * numb2 - saguing3;
+
+        System.out.println("10 * 4 + 6 =" + firstnum1);
+        System.out.println("(10-4) % 6 = " + firstnum2);
+        System.out.println("(10 + 4 + 6)/3 = " + firstnum3);
+        System.out.println("10 * 6 -(4 * 4 ) = " + firstnum4);
+
+    }
 }
